458e6ae6c6684e942854f6005dba3a1f1131c17b
murilogodoi1508-oss <murilo.godoi1508@gmail.com>
2026-02-26 17:37:42 -0300

    Update dados_primitivos.py

--- ALGORITMO-FLUXOGRAMA/LINGUAGEM PYTHON/dados_primitivos.py
+++ ALGORITMO-FLUXOGRAMA/LINGUAGEM PYTHON/dados_primitivos.py
@@ -22,3 +22,16 @@
 
 True
 False
+
+# Concatenação
+#É JUNTAR OS DADOS:
+print('olá mundo','Murilo')
+print('Resultado da soma: ', 2+2)
+print('Resultado da divisão' , 2/2)
+print('O aluno passou de ano?', True)
+print('Altura: ', 1.71)
+print('R$', 500.0)
+
+print('teste', 2, 5.2, False)
+
+print(1236 , 'teste')
