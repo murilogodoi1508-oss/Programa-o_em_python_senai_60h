98e0cefdddd57ad1cba59f08d97966b57831d907
murilogodoi1508-oss <murilo.godoi1508@gmail.com>
2026-02-26 17:25:12 -0300

    Add files via upload

---
+++ ALGORITMO-FLUXOGRAMA/LINGUAGEM PYTHON/aula_2.py
@@ -0,0 +1,8 @@
+# O interpretador sempre vai ignorar tudo o que estiver uma # na frente
+
+#Verificar se a instalação foi realizada corretamente
+
+#Testando a linguagem e as ferramentas
+
+print('Olá Mundo...')
+print('Teste')
---
+++ ALGORITMO-FLUXOGRAMA/LINGUAGEM PYTHON/dados_primitivos.py
@@ -0,0 +1,24 @@
+# Dados primitivos
+
+# Em Python
+
+#Tipo String - Texto
+'Texto'
+"Texto 2 hbejvhbfv"
+
+#Tipo interger - inteiro
+1
+2
+100
+
+#Float - Real
+
+5.0
+2.10
+100.
+0.5
+
+#Tipo bool - lógico
+
+True
+False
