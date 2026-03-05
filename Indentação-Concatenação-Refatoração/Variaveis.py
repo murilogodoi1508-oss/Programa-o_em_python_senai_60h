abe9c441510214fbda254367153933d5996b33c4
murilogodoi1508-oss <murilo.godoi1508@gmail.com>
2026-03-05 15:53:49 -0300

    Add files via upload

---
+++ Indentação-Concatenação-Refatoração/Variaveis.py
@@ -0,0 +1,64 @@
+# 1
+resultado = 17 % 4
+print(resultado)
+
+
+#2
+multiplicacao = 9 * 3
+resultado = multiplicacao ** 2
+print(f"O resultado de (9 * 3) ** 2 é: {resultado}")
+
+#3
+num1 = 81
+resultado = num1 ** 0,5
+print(f"o resultado de 81 ** 0,5 é: {resultado}")
+
+
+
+#4
+num2 = 20
+cont = 3 * 4
+print(f"O resultado é: {num2+cont}")
+
+
+#5
+
+multi = 15 * 2
+sub = 7
+print(f"o resultado é: {multi-sub}")
+
+
+#6
+
+resultado = 5 ** 3
+print(f"O resultado é: {resultado}")
+
+
+#7
+
+soma = 17+21+25
+div = 3
+print(f"O resultado é: {soma/div}")
+
+
+#8
+
+multi1 = 11 * 2
+soma1 = 7
+print(f"o resultado é: {multi1+soma1}")
+
+
+
+
+#9
+
+multi2 = 3 * 8
+sub1 = 15
+div2 = 2
+print(f"O resultado é: {(multi2-sub1)/div2}")
+
+#10
+
+resultado3 = 2 ** 10
+print(f"O resultado é: {resultado3}")
+
