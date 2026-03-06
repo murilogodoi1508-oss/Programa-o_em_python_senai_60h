4f3f401e631a6d9e260771f1fdae8e0385c24e09
murilogodoi1508-oss <murilo.godoi1508@gmail.com>
2026-03-06 17:12:05 -0300

    Add files via upload

--- aula7/aula7.py
+++ aula7/aula7.py
@@ -1,24 +1,42 @@
-#0
-numero = list(range(2,21,2))
-print(f"O resultado é: {numero}")
+e_commerce = {
 
-#1
-numeros = list(range(1,11))
-print(numeros)
+'secao':{
 
-
-#2
-print(numeros[2])
-
-#3
-numeros.append(9)
-print(numeros)
-
-#4
-numeros.remove(5)
-print(numeros)
+'tênis':{'Nike Air Max Invigor':600.0,'Nike TN':1000.0},
+'camisa':{'Camisa Oficial do São Paulo Futebol Clube':600.0,'Camisa oficial do Liverpool': 900.0},
+'acessorio':{'Cordão de Prata':50.0,'Pulseira de Prata':27.0},
 
 
-#5
-carros = ("Mercedes", "Lancer", "Opala")
-print = (carros)
+}
+}
+
+compras = []
+valor  = []
+secao = input('Digite a seção: ')
+prod1 =  input('Digite o produto: ')
+
+secao = input('Digite a seção: ')
+prod2 =  input('Digite o produto: ')
+
+secao = input('Digite a seção: ')
+prod3 =  input('Digite o produto: ')
+
+
+compras.append(prod1)
+compras.append(prod2)
+compras.append(prod3)
+print(compras)
+
+
+valor.append(e_commerce['secao'][secao][prod1])
+valor.append(e_commerce['secao'][secao][prod2])
+valor.append(e_commerce['secao'][secao][prod3])
+
+print('R$', valor)
+soma  =  sum(valor)
+print('R$', soma)
+
+
+
+
+
