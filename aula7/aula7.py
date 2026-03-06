1e492b9014bc34371cc0413240e59839d859a7b9
murilogodoi1508-oss <murilo.godoi1508@gmail.com>
2026-03-06 14:45:49 -0300

    Add files via upload

---
+++ aula7/aula7.py
@@ -0,0 +1,24 @@
+#0
+numero = list(range(2,21,2))
+print(f"O resultado é: {numero}")
+
+#1
+numeros = list(range(1,11))
+print(numeros)
+
+
+#2
+print(numeros[2])
+
+#3
+numeros.append(9)
+print(numeros)
+
+#4
+numeros.remove(5)
+print(numeros)
+
+
+#5
+carros = ("Mercedes", "Lancer", "Opala")
+print = (carros)
