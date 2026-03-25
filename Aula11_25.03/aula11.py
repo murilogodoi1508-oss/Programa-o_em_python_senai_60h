2b8f726ae48ebb37edeb340a79f4d96e25c4fdff
murilogodoi1508-oss <murilo.godoi1508@gmail.com>
2026-03-25 15:31:36 -0300

    Add files via upload

---
+++ Aula11_25.03/aula11.py
@@ -0,0 +1,49 @@
+
+#1
+while True:
+    try:
+       
+        num = int(input('Digite um número : '))
+        print(f'Você digitou o número: {num}')
+        break  
+    except ValueError:
+        
+        print('Error: Entrada inválida! Por favor, digite um número.')
+
+
+
+
+
+
+#2
+while True:
+    try:
+
+        num1 = int(input('Digite um primeiro número: '))
+        num2 = int(input('Digite um segundo número: '))
+        cont = num1 / num2
+        print(cont)
+        break
+    except ZeroDivisionError:
+        print('Error: Operação Inválida! Por favor, tente novamente.')
+
+
+
+#3
+
+try:
+       
+    lista1 = [10, 20, 30, 40, 50]
+    indice1 = 7
+    print(f"Elemento encontrado: {lista1[indice1]}")
+except IndexError:
+       print (f"Erro: Índice {indice1} é inválido para uma lista de tamanho {len(lista1)}.")
+
+print(lista1, indice1)
+print(lista1, 10)
+
+
+
+
+
+
